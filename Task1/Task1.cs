cee5c3537621717c7e1e0df9bbde80482c454644
Anastasia <bonnyn7@yandex.ru>
2022-07-26 11:32:23 +0300

    Added HW9

---
+++ Task1/Task1.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+using System;
+
+namespace Homework9
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int N = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Введите M: ");
+            int M = Convert.ToInt16(Console.ReadLine());
+            
+            Console.WriteLine($"N={N}, M={M}");
+
+            Solution (M,N);
+        }
+        static void Solution (int N, int M)
+        {
+            if (M > N)
+                for (int i = N; i <= M; i++)
+                    Console.Write($" {i}");
+            else
+                for (int i = M; i <= N; i++)
+                    Console.Write($" {i}");
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,45 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+using System;
+
+namespace Homework9
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            int m = InputNumbers("Введите m: ");
+            int n = InputNumbers("Введите n: ");
+            int temp = m;
+
+            if (m > n)
+            {
+                m = n;
+                n = temp;
+            }
+
+            PrintSum(m, n, temp = 0);
+            
+        }
+
+        static void PrintSum(int m, int n, int sum)
+        {
+            sum = sum + n;
+            if (n <= m)
+            {
+                Console.Write($"Сумма элементов= {sum} ");
+                return;
+            }
+            PrintSum(m, n - 1, sum);
+        }
+        static int InputNumbers (string input)
+        {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+        }
+
+    }
+}
